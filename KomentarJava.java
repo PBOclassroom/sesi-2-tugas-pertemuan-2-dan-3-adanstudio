2cf887ea54424fddb5b31cc9ff1d593038d62fe9
adanstudio <117730287+adanstudio@users.noreply.github.com>
2023-09-21 10:13:11 +0700

    Add files via upload

---
+++ KomentarJava.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Muhammad Irzan Ali
+ */
+public class KomentarJava {
+    public static void main(String[] args) {
+        System.out.println("Single line comment above");
+        //comment line 1
+        System.out.println("Multi line comment below");
+        /*comment line 2
+          comment line 3
+          comment line 4*/
+    }
+}
