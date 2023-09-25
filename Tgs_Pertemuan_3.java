9a9f93183da199f3bf462f5b0f6d90e08eb473df
adanstudio <117730287+adanstudio@users.noreply.github.com>
2023-09-25 10:57:06 +0700

    Add files via upload

---
+++ Tgs_Pertemuan_3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Muhammad Irzan Ali
+ */
+public class Tgs_Pertemuan_3 {
+     public static void main(String[] args) {
+        int Number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
+
+        System.out.println("Number = " + Number);
+        System.out.println("letter = " + letter);
+        System.out.println("result = " + result);
+        System.out.println("str = " + str);
+    }
+}
